a81b1211df1a3fe566b10cdef493dd7673adfbdc
nayzawoo <nayzawoo.me@gmail.com>
2026-04-10 15:27:22 +0630

    change outDir?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    outDir: "build",
+  },
   plugins: [
     react(),
     VitePWA({

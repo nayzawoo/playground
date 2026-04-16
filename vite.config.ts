51cede8bcb555b4e0139ee2d8cc2225969fe1ed6
nayzawoo <nayzawoo.me@gmail.com>
2026-04-16 07:58:56 +0630

    config for gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: isGhPages ? "/playground/" : "/",
   build: {
     outDir: "build",
   },
